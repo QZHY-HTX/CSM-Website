c05575cc59d72aa349e5791c31f55d7812881228
Einsteinww <awu@thallo.world>
2024-03-10 18:46:22 +0800

    update

---
+++ hackathon/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const app = express();
+const fs = require('fs');
+
+// 设置模板引擎为Pug  
+app.set('view engine', 'pug');
+
+// 设置静态文件路径
+app.use(express.static('public'));
+
+// 使用body-parser中间件来解析JSON请求体  
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// 创建数据库连接
+const db = mysql.createConnection({
+    host: '',
+    user: '',
+    password: '',
+    database: ''
+});
+
+// 连接数据库
+db.connect(err => {
+    if (err) {
+        throw err;
+    }
+    console.log("Connected to database");
+});
+
+// 渲染首页
+// 渲染首页
+app.get('/', (req, res) => {
+    db.query('SELECT team_name, team_number, team_menmbers, github FROM `hackathoncheck` ORDER BY team_name ASC', (err, results) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('index', { students: results });
+        }
+    });
+});
+
+const port = port;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
